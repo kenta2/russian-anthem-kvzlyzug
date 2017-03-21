ee702fc7b655c86f57ed2c5c893ff45f3ec950c5
Ken T Takusagawa <kenta@mit.edu>
2017-03-21 16:27:00 -0400

    generate the conductor score

--- 00-make-all.sh
+++ 00-make-all.sh
@@ -7,6 +7,9 @@ pushd lilypond
 perl ../scripts/book-gen.pl > "$partsfile"
 LANG=C lilypond "$partsfile"
 mv all-parts.pdf ../russian-national-anthem.pdf
+perl ../scripts/conductor-gen.pl > conductor.ly
+LANG=C lilypond conductor.ly
+mv conductor.pdf ..
 if [ "${keep_ly:-no}" != "yes" ]
-then rm "$partsfile"
+then rm "$partsfile" conductor.ly
 fi
